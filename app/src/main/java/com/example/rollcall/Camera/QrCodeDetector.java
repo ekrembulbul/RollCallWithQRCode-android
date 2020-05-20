8e7b37613f329cd5a987c7b504849ca636ff83c3
Ekrem BULBUL <l1115078@std.yildiz.edu.tr>
2020-05-20 19:29:48 +0300

    opencv test

--- app/src/main/java/com/example/rollcall/Camera/QrCodeDetector.java
+++ app/src/main/java/com/example/rollcall/Camera/QrCodeDetector.java
@@ -7,6 +7,7 @@ import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
@@ -15,23 +16,16 @@ import java.util.List;
 
 public class QrCodeDetector {
 
+    private int BLUR_VALUE = 3;
+    private float SQUARE_TOLERANCE = 0.15f;
+    private float AREA_TOLERANCE = 0.15f;
+    private float DISTANCE_TOLERANCE = 0.25f;
+    private int WARP_DIM = 300;
+    private int SMALL_DIM = 29;
+
     private final static String TAG = "QrCodeDetector";
 
     public void detect(Bitmap bitmap) {
-        Bitmap bmp32 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-        Mat mat = new Mat (bmp32.getHeight(), bmp32.getWidth(), CvType.CV_8UC4);
-        Utils.bitmapToMat(bmp32, mat);
-        Log.d(TAG, mat.toString());
-
-        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.bilateralFilter(mat, mat, 11, 17, 17);
-        Imgproc.GaussianBlur(mat, mat, new Size(3, 3), 0);
-        Imgproc.Canny(mat, mat, 30, 200);
-
-        List<MatOfPoint> counters = new ArrayList<>();
-        Mat hierarchy = new Mat ();
-        Imgproc.findContours(mat, counters, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
         
     }
 }
